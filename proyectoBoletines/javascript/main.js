6ae5edf6693e4c38963e53707dd54ccde6a7800a
edgaxerdeveloper <edgaxer@gmail.com>
2021-08-30 19:42:20 -0300

    Update main.js

--- proyectoBoletines/javascript/main.js
+++ proyectoBoletines/javascript/main.js
@@ -29,7 +29,7 @@ links.forEach(function (link) {
 		content.classList.add("animate__animated");
 
 		setTimeout(function () {
-			location.href = "/";
+			location.href = "/proyectoBoletines";
 		},600);
 
 		return false;
